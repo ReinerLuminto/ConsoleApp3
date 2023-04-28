57684da3bdf5268506cb53a5b49b96ce1ddd1f8e
ReinerLuminto <reiner.andrew.luminto@gmail.com>
2023-04-28 14:05:44 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(func(1));
+        }
+
+        static bool func(int i)
+        {
+            return true;
+        }
+    }
+
+
+}
